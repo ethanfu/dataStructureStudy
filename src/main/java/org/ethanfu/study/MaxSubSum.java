29db51770abdd2aee20ed89b6b0156a6538b5943
ethan.fu1 <ethanfu@ethanfu-HP-ProBook-6450b.(none)>
2012-08-22 22:50:32 +0800

    modify my maxSubSum function

--- src/main/java/org/ethanfu/study/MaxSubSum.java
+++ src/main/java/org/ethanfu/study/MaxSubSum.java
@@ -4,10 +4,15 @@ public class MaxSubSum {
 
 	public static int maxSubSum(int[] a){
 		
-		int maxSum = 0;
+		int maxSum = 0,thisSum=0;
 		
 		for(int i=0; i<a.length; i++){
-			
+			thisSum += a[i];
+			if(thisSum>maxSum){
+				maxSum = thisSum;
+			}else if(thisSum<0){
+				thisSum = 0;
+			}
 		}
 		return maxSum;
 	}
