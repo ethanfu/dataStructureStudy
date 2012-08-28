e7d4b2e2585e546ee52e6e3f92a436d109164649
ethan.fu1 <ethanfu@ethanfu-HP-ProBook-6450b.(none)>
2012-08-28 10:14:46 +0800

    add Student for hashmap key implement

--- src/main/java/org/ethanfu/study/App.java
+++ src/main/java/org/ethanfu/study/App.java
@@ -1,13 +1,44 @@
 package org.ethanfu.study;
 
+import java.util.LinkedList;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println(permutation("AABCDDE"));
+	}
+
+	public static LinkedList<String> permutation(String str) {
+
+		LinkedList<String> linkedString = new LinkedList<String>();
+		if (str.length() <= 1) {
+			linkedString.add(str);
+			return linkedString;
+		}
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			// consider the case in which the characters may be duplicated.
+			if (i > 0 && ch == str.charAt(i - 1)) {
+				continue;
+			}
+			String newStr = remove(str, i);
+			LinkedList<String> newStrList = permutation(newStr);
+			for (int j = 0; j < newStrList.size(); j++) {
+				linkedString.add(ch + newStrList.get(j));
+			}
+		}
+		return linkedString;
+	}
+
+	// remove the ith character from the string
+	public static String remove(String str, int i) {
+		if (i == 0)
+			return str.substring(1, str.length());
+		if (i == str.length() - 1)
+			return str.substring(0, i);
+		return str.substring(0, i) + str.substring(i + 1, str.length());
+	}
 }
